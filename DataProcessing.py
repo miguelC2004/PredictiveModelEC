cc9e9fd09ae851484a7f784269d4b06eb3554e84
Miguel Catano <118161067+miguelC2004@users.noreply.github.com>
2024-08-11 16:00:47 -0500

    Update DataProcessing.py

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,7 +1,6 @@
 import polars as pl
 
 def ProcessData(DataFrame: pl.DataFrame) -> pl.DataFrame:
-    """Procesa los datos y retorna un DataFrame listo para el modelo predictivo."""
     processedDataFrame = DataFrame.with_columns([
         (pl.col('Quantity') * pl.col('UnitPrice')).alias('TotalAmount')
     ])
