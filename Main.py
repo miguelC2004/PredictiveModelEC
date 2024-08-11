6957f38532834aab078cd6d698cc93fe75185a4b
Miguel Catano <118161067+miguelC2004@users.noreply.github.com>
2024-08-11 16:03:45 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,8 @@
+from PredictiveModel import BuildAndTrainModel
+
+def Main():
+    """Main function to run the predictive model workflow"""
+    BuildAndTrainModel()
+
+if __name__ == "__main__":
+    Main()
